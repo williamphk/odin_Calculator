fc9034594fb23715022d0a447947bdff10a3a8eb
williamphk <65807958+williamphk@users.noreply.github.com>
2022-05-16 20:32:09 +0800

    Add delete button

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let mainDisplay = document.getElementById("mainDisplay");
 let subDisplay = document.getElementById("subDisplay");
 
-let num1 = 0; //answer
-let num2 = ""; //display
-let sign = "=";
+let num1_DEFAULT = 0;
+let num2_DEFAULT = "";
+let sign_DEFAULT = "=";
+
+let num1 = num1_DEFAULT; //answer
+let num2 = num2_DEFAULT; //display
+let sign = sign_DEFAULT;
 
 function operate(operator) {
   if (operator != "=" && sign === "=") {
     console.log("operator1", num1, num2, sign);
     sign = operator;
     num1 = Number(mainDisplay.innerHTML);
-    num2 = "";
+    num2 = num2_DEFAULT;
     subDisplay.innerHTML += `${sign}`;
   } else if (operator == "=") {
     console.log("operator2", num1, num2, sign);
@@ -32,14 +36,21 @@ function operate(operator) {
         divide();
         break;
     }
-    num2 = "";
+    num2 = num2_DEFAULT;
     sign = operator;
   } else {
+    sign = operator;
     console.log("operator3", num1, num2, sign);
     switch (sign) {
       case "+":
         if (subDisplay.innerHTML.slice(-1) === "+") {
           subDisplay.innerHTML += ``;
+        } else if (
+          subDisplay.innerHTML.slice(-1) === "-" ||
+          subDisplay.innerHTML.slice(-1) === "x" ||
+          subDisplay.innerHTML.slice(-1) === "÷"
+        ) {
+          subDisplay.innerHTML = `${sign}`;
         } else {
           subDisplay.innerHTML += `${sign}`;
         }
@@ -48,6 +59,12 @@ function operate(operator) {
       case "-":
         if (subDisplay.innerHTML.slice(-1) === "-") {
           subDisplay.innerHTML += ``;
+        } else if (
+          subDisplay.innerHTML.slice(-1) === "+" ||
+          subDisplay.innerHTML.slice(-1) === "x" ||
+          subDisplay.innerHTML.slice(-1) === "÷"
+        ) {
+          subDisplay.innerHTML = `${sign}`;
         } else {
           subDisplay.innerHTML += `${sign}`;
         }
@@ -56,21 +73,36 @@ function operate(operator) {
       case "x":
         if (subDisplay.innerHTML.slice(-1) === "x") {
           subDisplay.innerHTML += ``;
+          console.log("operator3x", num1, num2, sign);
+        } else if (
+          subDisplay.innerHTML.slice(-1) === "+" ||
+          subDisplay.innerHTML.slice(-1) === "-" ||
+          subDisplay.innerHTML.slice(-1) === "÷"
+        ) {
+          subDisplay.innerHTML = `${sign}`;
+          console.log("operator3xx", num1, num2, sign);
         } else {
           subDisplay.innerHTML += `${sign}`;
+          console.log("operator3xxx", num1, num2, sign);
+          multiply();
         }
-        multiply();
         break;
       case "÷":
         if (subDisplay.innerHTML.slice(-1) === "÷") {
           subDisplay.innerHTML += ``;
+        } else if (
+          subDisplay.innerHTML.slice(-1) === "+" ||
+          subDisplay.innerHTML.slice(-1) === "-" ||
+          subDisplay.innerHTML.slice(-1) === "x"
+        ) {
+          subDisplay.innerHTML = `${sign}`;
         } else {
           subDisplay.innerHTML += `${sign}`;
+          divide();
         }
-        divide();
         break;
     }
-    num2 = "";
+    num2 = num2_DEFAULT;
     sign = operator;
   }
 }
@@ -93,22 +125,30 @@ function subtract() {
 }
 
 function multiply() {
-  num1 *= Number(num2);
   if (String(num1).length > 10) {
     console.log("multiplay1", num1, num2, sign);
+    num1 *= Number(num2);
     mainDisplay.innerHTML = String(num1).substring(0, 11);
-  } else {
+  } else if (num2 == "") {
     console.log("multiplay2", num1, num2, sign);
+    num1 *= Number(num1);
     mainDisplay.innerHTML = num1;
-    console.log("multiplay2e", num1, num2, sign);
+  } else {
+    console.log("multiplay3", num1, num2, sign);
+    num1 *= Number(num2);
+    mainDisplay.innerHTML = num1;
+    console.log("multiplay3e", num1, num2, sign);
   }
 }
 
 function divide() {
-  if (num1 == "0" && num2 == "0") {
-    mainDisplay.innerHTML = "Undefined";
+  if ((num1 == "0" && num2 == "0") || (num1 == "0" && num2 == "")) {
+    mainDisplay.innerHTML = "<font size='5'>Result is Undefined</font>";
   } else if (num2 == "0") {
     mainDisplay.innerHTML = "<font size='6'>Cannot divide by 0</font>";
+  } else if (num2 == "") {
+    num1 /= Number(num1);
+    mainDisplay.innerHTML = num1;
   } else {
     num1 /= Number(num2);
     if (String(num1).length > 10) {
@@ -145,9 +185,32 @@ function displayNum(x) {
 
 function reset() {
   console.log("reset", num1, num2, sign);
-  num1 = 0;
-  num2 = "";
-  sign = "=";
+  num1 = num1_DEFAULT;
+  num2 = num2_DEFAULT;
+  sign = sign_DEFAULT;
   mainDisplay.innerHTML = 0;
   subDisplay.innerHTML = "";
+}
+
+function deleteNum() {
+  if (
+    subDisplay.innerHTML.slice(-1) == "+" ||
+    subDisplay.innerHTML.slice(-1) == "-" ||
+    subDisplay.innerHTML.slice(-1) == "x" ||
+    subDisplay.innerHTML.slice(-1) == "÷"
+  ) {
+    subDisplay.innerHTML = subDisplay.innerHTML.slice(0, -1);
+    sign = sign_DEFAULT;
+    console.log("delete1", num1, num2, sign);
+  } else if (mainDisplay.innerHTML.length > 1) {
+    mainDisplay.innerHTML = mainDisplay.innerHTML.slice(0, -1);
+    subDisplay.innerHTML = subDisplay.innerHTML.slice(0, -1);
+    num2 = num2.slice(0, -1);
+    console.log("delete1", num1, num2, sign);
+  } else {
+    mainDisplay.innerHTML = 0;
+    subDisplay.innerHTML = "";
+    num2 = num2_DEFAULT;
+    console.log("delete2", num1, num2, sign);
+  }
 }
